299ef886ade442c1b83e270a2673b80a3bcca3c5
chris <christoph.tetzlaff@ct360.de>
2016-06-26 13:54:42 +0200

    The new element

---
+++ LibStandard/LibStandard/Element.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibStandard
+{
+    class Element
+    {
+        /// <summary>
+        /// Its index
+        /// </summary>
+        public int Index { get; set; }
+    }
+}
